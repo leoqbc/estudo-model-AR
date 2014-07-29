d280f969516926c35c3ae7e1cdfa7ae2a92cab4c
Leonardo Tumadjian <tumadjian@gmail.com>
2014-07-28 23:04:38 -0300

    Update Model.php
    Corrige indentação!

--- lib/Model.php
+++ lib/Model.php
@@ -1,58 +1,56 @@
-<?php
-
 // Este model depende da Classe Banco
 class Model {
-	protected static $tabela;
-	protected static $id_column = 'id';
-	public static $status;
-	const NOVO  = 1;
-	const USADO = 2;
+    protected static $tabela;
+    protected static $id_column = 'id';
+    public static $status;
+    const NOVO  = 1;
+    const USADO = 2;
 
-	public function __construct ()
+    public function __construct ()
     {
-		static::$status = self::NOVO;
-	}
+        static::$status = self::NOVO;
+    }
 
-	public static function loadModel (StdClass $res)
-	{
-		$model = new static();
-		static::$status = self::USADO;
-		foreach ($res as $prop => $val) {
-			$model->$prop = $val;
-		}
-		return $model;
-	}
-
-	public static function getByid($id, $campos='*') 
-	{
-		$db = Banco::instanciar();
-		$where = static::$id_column . '=' . $id;
-		$res = $db->ver(static::$tabela, $campos,  $where);
-
-		if ($res) {
-			return static::loadModel($res);
-		}
-		return false;
-	}
-
-	public static function getAll($campos='*', $where=null)
-	{
-		$db = Banco::instanciar();
-		$results = $db->listar(static::$tabela, $campos, $where);
-		if (count ($results)) {
-			$id = static::$id_column;
-			foreach ($results as $obj) {
-				$model = static::loadModel($obj);
-				$storage[$model->$id] = $model; 
-			}
-			return $storage;
-		}
-		return false;
-	}
-
-	public function save()
+    public static function loadModel (StdClass $res)
     {
-		$db = Banco::instanciar();
+        $model = new static();
+        static::$status = self::USADO;
+        foreach ($res as $prop => $val) {
+            $model->$prop = $val;
+        }
+        return $model;
+    }
+
+    public static function getByid($id, $campos='*')
+    {
+        $db = Banco::instanciar();
+        $where = static::$id_column . '=' . $id;
+        $res = $db->ver(static::$tabela, $campos,  $where);
+
+        if ($res) {
+            return static::loadModel($res);
+        }
+        return false;
+    }
+
+    public static function getAll($campos='*', $where=null)
+    {
+        $db = Banco::instanciar();
+        $results = $db->listar(static::$tabela, $campos, $where);
+        if (count ($results)) {
+            $id = static::$id_column;
+            foreach ($results as $obj) {
+                $model = static::loadModel($obj);
+                $storage[$model->$id] = $model;
+            }
+            return $storage;
+        }
+        return false;
+    }
+
+    public function save()
+    {
+        $db = Banco::instanciar();
         if ($this->validate()) {
             $this->beforeSave();
             if (static::$status === self::USADO) {
@@ -65,18 +63,18 @@ class Model {
             $this->afterSave();
             return true;
         }
-		return false;
-	}
+        return false;
+    }
 
-	public function delete() 
-	{
-		$db = Banco::instanciar();
-		if (static::$status === self::USADO) {
-			$id = static::$id_column;
-			$db->apagar(static::$tabela, array($id => $this->$id));
-			return true;
-		}
-	}
+    public function delete()
+    {
+        $db = Banco::instanciar();
+        if (static::$status === self::USADO) {
+            $id = static::$id_column;
+            $db->apagar(static::$tabela, array($id => $this->$id));
+            return true;
+        }
+    }
 
     /*
     * TODO:
