f39bf6d75f9559da876f83ac0e8a1509b7fe1ff9
Leonardo Tumadjian <tumadjian@gmail.com>
2014-07-28 23:07:18 -0300

    Update bootstrap.php
    Corrige indentação

--- bootstrap.php
+++ bootstrap.php
@@ -2,24 +2,24 @@
 
 function loadLibs($class) 
 {
-	$dir = dirname(__FILE__);
-	$file = "$dir/lib/$class.php";
+    $dir = dirname(__FILE__);
+    $file = "$dir/lib/$class.php";
 
-	if (file_exists($file)) {
-		require_once($file);
-		return true;
-	}
+    if (file_exists($file)) {
+        require_once($file);
+        return true;
+    }
 }
 
 function loadModels($class) 
 {
-	$dir = dirname(__FILE__);
-	$file = "$dir/models/$class.php";
+    $dir = dirname(__FILE__);
+    $file = "$dir/models/$class.php";
 
-	if (file_exists($file)) {
-		require_once($file);
-		return true;
-	}
+    if (file_exists($file)) {
+	require_once($file);
+	return true;
+    }
 }
 
 // Registra as pastas model e lib para
